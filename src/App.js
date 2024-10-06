d71786e0a82c0257cff13c2951d6aaeea02b089a
Dulara-Sasanka <dularasasanka8@gmail.com>
2024-10-06 10:34:24 +0530

    develop components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+ import React from "react";
+import Nameprinter from "./components/Nameprinter";
 function App() {
+  // const names = ["Sri Lanka","India","Nepal","Australia"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nameprinter name = {"Afraj"}/>
+      <Nameprinter name = {"Dulara"}/>
+      <Nameprinter name = {"Shiran"}/>
     </div>
   );
 }
